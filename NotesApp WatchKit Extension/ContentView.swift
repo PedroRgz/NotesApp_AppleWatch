c76a00846b1e865be42e216db1e2daa0f2bfc644
Pedro Rodríguez <p_alex96@hotmail.com>
2021-07-10 18:41:16 -0500

    Initial Commit

---
+++ NotesApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by Pedro Rodríguez on 10/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesApp WatchKit Extension/NotesAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesAppApp.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by Pedro Rodríguez on 10/07/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
